1693ecbac1fbcd706d17e5f921eacbb5820803bc
Geolffrey Mena <geolffrey@hotmail.com>
2023-06-17 15:48:24 -0400

    Initial commit

---
+++ Extensions/JSRuntimeExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Server.Extensions
+{
+    public static class JSRuntimeExtension
+    {
+        public static async Task<IJSObjectReference> ComponentModule<T>(this IJSRuntime jsRuntime)
+        {
+            var typeOf = typeof(T);
+            var stringBuilder = new StringBuilder("./");
+            stringBuilder.Append(typeOf.FullName?.Remove(0, typeOf.Assembly.GetName().Name.Length + 1).Replace(".", "/"));
+            stringBuilder.Append(".razor.js");
+
+            var result = await jsRuntime.InvokeAsync<IJSObjectReference>("import", stringBuilder.ToString());
+
+            return result;
+        }
+    }
+}
---
+++ Pages/CirclePacking.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Blazor.Server.Extensions;
+using Microsoft.AspNetCore.Components.Web;
+using System.Reflection;
+
+namespace Blazor.Server.Pages
+{
+    [Route("/circle-packing")]
+    public sealed partial class CirclePacking : ComponentBase, IAsyncDisposable
+    {
+        [Inject] IJSRuntime _JSRuntime { get; set; } = default!;
+        private IJSObjectReference _Module = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                try
+                {
+                    _Module = await _JSRuntime.ComponentModule<CirclePacking>();
+                    await _Module.InvokeVoidAsync("init");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void onClickZoomIn(MouseEventArgs args) => _Module.InvokeVoidAsync("onclickZoomIn");
+        private void onClickZoomOut(MouseEventArgs args) => _Module.InvokeVoidAsync("onclickZoomOut");
+
+        private void onClickZoomRandom(MouseEventArgs args) => _Module.InvokeVoidAsync("onclickzoomRandom");
+
+        private void onClickZoomReset(MouseEventArgs args) => _Module.InvokeVoidAsync("onclickzoomReset");
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            if (_Module is not null)
+            {
+                await _Module.InvokeVoidAsync("dispose");
+                await _Module.DisposeAsync();
+            }
+        }
+    }
+}
